d8b93297ae358c6f4daeda5585407bc67e871035
Andris <undris01@gmail.com>
2024-10-02 20:28:00 +0200

    10.02

--- orszagok/orszagok_feladat.sql
+++ orszagok/orszagok_feladat.sql
@@ -19,26 +19,49 @@ GDP N 5 egy főre jutó bruttó hazai termék USA dollárban
 Kat N 1 ismertségi kategória (1,2,3)
 */
 
+SELECT * FROM orszagok;
 -- 1. Mi MADAGASZKÁR fővárosa?
+SELECT fovaros, orszag FROM orszagok WHERE orszag LIKE 'MADAGASZKÁR';
 -- 2. Melyik ország fővárosa OUAGADOUGOU?
+SELECT orszag FROM orszagok WHERE fovaros LIKE 'OUAGADOUGOU';
 -- 3. Melyik ország autójele a TT?
+SELECT orszag, autojel FROM orszagok WHERE autojel LIKE 'TT ';
 -- 4. Melyik ország pénzének jele az SGD?
+SELECT orszag FROM orszagok WHERE penzjel LIKE 'SGD';
 -- 5. Melyik ország nemzetközi telefon-hívószáma a 61?
+SELECT orszag FROM orszagok WHERE telefon = 61;
 -- 6. Mekkora területű Monaco?
+SELECT terulet FROM orszagok WHERE orszag = 'MONACO';
 -- 7. Hányan laknak Máltán?
+SELECT nepesseg * 1000 AS málta_népessége FROM orszagok WHERE orszag = 'MÁLTA';
 -- 8. Mennyi Japán népsűrűsége?
+SELECT nepesseg * 1000 / terulet AS p/km FROM orszagok WHERE orszag = 'JAPÁN';
 -- 9. Hány lakosa van a Földnek?
+SELECT SUM(nepesseg * 1000)  AS föld_nepessége FROM orszagok;
+SELECT SUM(nepesseg) * 1000  AS föld_nepessége FROM orszagok;
 -- 10. Mennyi az országok területe összesen?
+SELECT SUM(terulet) AS osszes_terulet FROM orszagok;
 -- 11. Mennyi az országok átlagos népessége?
+SELECT ROUND(AVG(nepesseg)) AS atlag_nepesseg FROM orszagok;
 -- 12. Mennyi az országok átlagos területe?
+SELECT AVG(terulet) AS A FROM orszagok;
 -- 13. Mennyi a Föld népsűrűsége?
+SELECT SUM(nepesseg) * 1000 / SUM(terulet) FROM orszagok;
 -- 14. Hány 1.000.000 km2-nél nagyobb területű ország van?
+SELECT COUNT(orszag) FROM orszagok WHERE terulet > 1000000;
 -- 15. Hány 100 km2-nél kisebb területű ország van?
+SELECT COUNT(*) FROM orszagok WHERE terulet < 100;
 -- 16. Hány 20.000 főnél kevesebb lakosú ország van?
+SELECT COUNT(*) FROM orszagok WHERE nepesseg < 20;
 -- 17. Hány országra igaz, hogy területe kisebb 100 km2-nél, vagy pedig a lakossága kevesebb 20.000 főnél?
+SELECT COUNT(*) FROM orszagok WHERE terulet < 100000 OR nepesseg < 20;
 -- 18. Hány ország területe 50.000 és 150.000 km2 közötti?
+SELECT COUNT(*) FROM orszagok WHERE terulet >= 50000 AND terulet <= 150000;
+SELECT COUNT(*) FROM orszagok WHERE terulet BETWEEN 50000 AND  150000;
 -- 19. Hány ország lakossága 8 és 12 millió közötti?
+SELECT COUNT(*) FROM orszagok WHERE nepesseg BETWEEN 8000 AND 12000;
 -- 20. Mely fővárosok népesebbek 20 millió főnél?
+
 -- 21. Mely országok népsűrűsége nagyobb 500 fő/km2-nél?
 -- 22. Hány ország államformája köztársaság?
 -- 23. Mely országok pénzneme a kelet-karib dollár?
